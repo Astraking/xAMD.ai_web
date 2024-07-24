2d01640490dd3174ccee120005ba256832efdd61
Kofi Adjei-Boateng <85785115+Astraking@users.noreply.github.com>
2024-07-24 08:29:24 +0000

    Update app.py resize
    Changed transform resize to 128 * 128

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ amd_model = load_model(AMDModel, amd_model_path)
 
 # Define image transformations
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((128, 128)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
